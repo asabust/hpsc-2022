dbc13ce0b67e2508636347d448a7ad4fc983c2b6
Erin Yang <asabust@users.noreply.github.com>
2022-04-20 23:06:50 +0900

    assignment 1 Homework1

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,12 +1,18 @@
 #include <cstdio>
+#include <omp.h>
 
-int main() {
+int main()
+{
   int n = 10;
   double dx = 1. / n;
   double pi = 0;
-  for (int i=0; i<n; i++) {
+
+#pragma omp parallel for
+  for (int i = 0; i < n; i++)
+  {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
+    // printf("%d : %d pi:%.1lf\n", omp_get_thread_num(),i,pi);
   }
-  printf("%17.15f\n",pi);
+  printf("%17.15f\n", pi);
 }
